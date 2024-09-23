ef6c8672a02dfa356c8213faabb554310206e99d
thinkyside <eunlyuing@gmail.com>
2024-09-23 14:13:04 +0900

    PUSH

---
+++ PRTest/PRTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PRTest
+//
+//  Created by 김민준 on 9/23/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PRTest/PRTest/PRTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  PRTestApp.swift
+//  PRTest
+//
+//  Created by 김민준 on 9/23/24.
+//
+
+import SwiftUI
+
+@main
+struct PRTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
